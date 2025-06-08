9a94d0ae74239f52b5a343ca9c26d1d4339bfcf3
Shreyash <shreyashiitr22@gmail.com>
2025-06-08 22:54:47 +0530

    fixed he login api

--- my-app/src/app/api/users/login/route.ts
+++ my-app/src/app/api/users/login/route.ts
@@ -2,6 +2,7 @@ import {connect} from '@/dbConfig/dbConfig'
 import User from '@/models/userModel'
 import { NextRequest, NextResponse } from 'next/server'
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 connect()
 
 export async function POST(request:NextRequest,response:NextResponse){
@@ -15,7 +16,17 @@ const validPassword=await bcrypt.compare(password,user.password)
 if(!validPassword){
     return NextResponse.json({message:"Invalid Password"},{status:400})
 }
+const tokenData={
+    id:user._id,
+    username:user.username,
+    email:user.email
+}
 
+if (!process.env.TOKEN_SECRET) {
+    throw new Error('TOKEN_SECRET is not defined in environment variables');
+}
+
+const token = jwt.sign(tokenData, process.env.TOKEN_SECRET, {expiresIn:"1h"});
 
 }
 catch(e:any){
